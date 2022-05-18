261fd433a2412939b6e4951b7b9d7d15036c2a21
rodomedina <49995661+rodomedina@users.noreply.github.com>
2022-05-18 18:22:28 -0300

    Proyecto-2014

---
+++ frmGeneradorBarras.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmGeneradorBarras
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmGeneradorBarras))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AyudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AcercadeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Muestra = New System.Windows.Forms.PictureBox()
+        Me.CodigoBarra = New MyBarcode.EAN13Barcode()
+        Me.NombreArchivo = New System.Windows.Forms.TextBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.BtnGenerar = New DevComponents.DotNetBar.ButtonX()
+        Me.LabelX1 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX2 = New DevComponents.DotNetBar.LabelX()
+        Me.txtinicio = New DevComponents.DotNetBar.Controls.TextBoxX()
+        Me.txtfinal = New DevComponents.DotNetBar.Controls.TextBoxX()
+        Me.ep = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.Muestra, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ep, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem, Me.AyudaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(423, 24)
+        Me.MenuStrip1.TabIndex = 23
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SalirToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.ArchivoToolStripMenuItem.Text = "&Archivo"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(96, 22)
+        Me.SalirToolStripMenuItem.Text = "&Salir"
+        '
+        'AyudaToolStripMenuItem
+        '
+        Me.AyudaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AcercadeToolStripMenuItem})
+        Me.AyudaToolStripMenuItem.Name = "AyudaToolStripMenuItem"
+        Me.AyudaToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.AyudaToolStripMenuItem.Text = "Ay&uda"
+        '
+        'AcercadeToolStripMenuItem
+        '
+        Me.AcercadeToolStripMenuItem.Name = "AcercadeToolStripMenuItem"
+        Me.AcercadeToolStripMenuItem.Size = New System.Drawing.Size(135, 22)
+        Me.AcercadeToolStripMenuItem.Text = "&Acerca de..."
+        '
+        'Muestra
+        '
+        Me.Muestra.BackColor = System.Drawing.Color.White
+        Me.Muestra.Location = New System.Drawing.Point(420, 30)
+        Me.Muestra.Name = "Muestra"
+        Me.Muestra.Size = New System.Drawing.Size(194, 149)
+        Me.Muestra.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Muestra.TabIndex = 11
+        Me.Muestra.TabStop = False
+        '
+        'CodigoBarra
+        '
+        Me.CodigoBarra.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.CodigoBarra.BackColor = System.Drawing.Color.White
+        Me.CodigoBarra.BarHeight = 0.0R
+        Me.CodigoBarra.BarWidth = 0.0R
+        Me.CodigoBarra.Font = New System.Drawing.Font("Arial", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CodigoBarra.Location = New System.Drawing.Point(214, 26)
+        Me.CodigoBarra.Margin = New System.Windows.Forms.Padding(0)
+        Me.CodigoBarra.Name = "CodigoBarra"
+        Me.CodigoBarra.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.CodigoBarra.ShowBarcodeText = True
+        Me.CodigoBarra.ShowCheckSum = True
+        Me.CodigoBarra.Size = New System.Drawing.Size(197, 142)
+        Me.CodigoBarra.TabIndex = 4
+        Me.CodigoBarra.Value = "000000000000"
+        '
+        'NombreArchivo
+        '
+        Me.NombreArchivo.Location = New System.Drawing.Point(4, 127)
+        Me.NombreArchivo.Name = "NombreArchivo"
+        Me.NombreArchivo.Size = New System.Drawing.Size(17, 20)
+        Me.NombreArchivo.TabIndex = 24
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 190)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(423, 22)
+        Me.StatusStrip1.TabIndex = 25
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'BtnGenerar
+        '
+        Me.BtnGenerar.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.BtnGenerar.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground
+        Me.BtnGenerar.Location = New System.Drawing.Point(46, 124)
+        Me.BtnGenerar.Name = "BtnGenerar"
+        Me.BtnGenerar.Size = New System.Drawing.Size(154, 23)
+        Me.BtnGenerar.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
+        Me.BtnGenerar.TabIndex = 30
+        Me.BtnGenerar.Text = "Generar Secuencia"
+        '
+        'LabelX1
+        '
+        '
+        '
+        '
+        Me.LabelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelX1.Location = New System.Drawing.Point(11, 72)
+        Me.LabelX1.Name = "LabelX1"
+        Me.LabelX1.Size = New System.Drawing.Size(29, 23)
+        Me.LabelX1.TabIndex = 31
+        Me.LabelX1.Text = "Inicio"
+        '
+        'LabelX2
+        '
+        '
+        '
+        '
+        Me.LabelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelX2.Location = New System.Drawing.Point(11, 95)
+        Me.LabelX2.Name = "LabelX2"
+        Me.LabelX2.Size = New System.Drawing.Size(29, 23)
+        Me.LabelX2.TabIndex = 32
+        Me.LabelX2.Text = "Final"
+        '
+        'txtinicio
+        '
+        '
+        '
+        '
+        Me.txtinicio.Border.Class = "TextBoxBorder"
+        Me.txtinicio.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.txtinicio.Location = New System.Drawing.Point(46, 72)
+        Me.txtinicio.Name = "txtinicio"
+        Me.txtinicio.Size = New System.Drawing.Size(154, 20)
+        Me.txtinicio.TabIndex = 33
+        '
+        'txtfinal
+        '
+        '
+        '
+        '
+        Me.txtfinal.Border.Class = "TextBoxBorder"
+        Me.txtfinal.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.txtfinal.Location = New System.Drawing.Point(46, 95)
+        Me.txtfinal.Name = "txtfinal"
+        Me.txtfinal.Size = New System.Drawing.Size(154, 20)
+        Me.txtfinal.TabIndex = 34
+        '
+        'ep
+        '
+        Me.ep.ContainerControl = Me
+        '
+        'frmGeneradorBarras
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(423, 212)
+        Me.Controls.Add(Me.txtfinal)
+        Me.Controls.Add(Me.txtinicio)
+        Me.Controls.Add(Me.LabelX2)
+        Me.Controls.Add(Me.LabelX1)
+        Me.Controls.Add(Me.BtnGenerar)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.NombreArchivo)
+        Me.Controls.Add(Me.CodigoBarra)
+        Me.Controls.Add(Me.Muestra)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(439, 250)
+        Me.MinimumSize = New System.Drawing.Size(439, 250)
+        Me.Name = "frmGeneradorBarras"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Credenciales - Generador de Barras Secuencial"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.Muestra, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ep, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AyudaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AcercadeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Muestra As System.Windows.Forms.PictureBox
+    Friend WithEvents CodigoBarra As MyBarcode.EAN13Barcode
+    Friend WithEvents NombreArchivo As System.Windows.Forms.TextBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents BtnGenerar As DevComponents.DotNetBar.ButtonX
+    Friend WithEvents LabelX1 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX2 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents txtinicio As DevComponents.DotNetBar.Controls.TextBoxX
+    Friend WithEvents txtfinal As DevComponents.DotNetBar.Controls.TextBoxX
+    Friend WithEvents ep As System.Windows.Forms.ErrorProvider
+
+End Class
---
+++ frmGeneradorBarras.vb
@@ -0,0 +1,191 @@
+﻿Imports System.IO
+
+Public Class frmGeneradorBarras
+
+    Dim num As Integer
+
+    Private Function getBitmap(ByVal pCtrl As Control) As Drawing.Bitmap
+
+        Dim myBmp As New Bitmap(pCtrl.Width, pCtrl.Height)
+        Dim g As Graphics = Graphics.FromImage(myBmp)
+
+        Dim p As New Point(pCtrl.Parent.Width - pCtrl.Parent.ClientRectangle.Width, pCtrl.Parent.Height - pCtrl.Parent.ClientRectangle.Height)
+        g.CopyFromScreen(pCtrl.Parent.Location + pCtrl.Location + p, Point.Empty, myBmp.Size)
+
+        Return myBmp
+        g.Dispose()
+
+    End Function
+
+    Private Sub SalirToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles SalirToolStripMenuItem.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub frmGeneradorBarras_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        Muestra.Visible = False
+        NombreArchivo.Visible = False
+        Muestra.Width = CodigoBarra.Width
+        Muestra.Height = CodigoBarra.Height
+        txtinicio.Focus()
+
+    End Sub
+
+    Function SoloNumeros(ByVal Keyascii As Short) As Short
+
+        If InStr("1234567890", Chr(Keyascii)) = 0 Then
+            SoloNumeros = 0
+        Else
+            SoloNumeros = Keyascii
+        End If
+        Select Case Keyascii
+            Case 8
+                SoloNumeros = Keyascii
+            Case 13
+                SoloNumeros = Keyascii
+        End Select
+
+    End Function
+
+    Public Sub verificarFolder(rutaDirectorio)
+
+        If Not Directory.Exists(rutaDirectorio) Then
+            Directory.CreateDirectory(rutaDirectorio)
+        End If
+
+    End Sub
+
+    Private Sub BtnGenerar_Click(sender As System.Object, e As System.EventArgs) Handles BtnGenerar.Click
+        ep.Clear()
+
+        If txtinicio.Text = "" Then
+            ep.SetError(txtinicio, "Debe ingresar un Limite inicial para la secuencia de barras.")
+            Return
+        End If
+
+        If txtfinal.Text = "" Then
+            ep.SetError(txtfinal, "Debe ingresar un Limite final para la secuencia de barras.")
+            Return
+        End If
+
+        If Val(txtinicio.Text) > Val(txtfinal.Text) Then
+            ep.SetError(txtinicio, "El limite inicial debe ser menor o igual al final.")
+            Return
+        End If
+
+
+        If Val(txtinicio.Text) > Val(txtfinal.Text) Then
+
+
+        End If
+        '--- Variables
+        Dim a As Integer
+        NombreArchivo.Text = 0
+
+        '--- Verifico Directorio y creo si es necesario
+        If Not Directory.Exists("c:/barras generadas") Then
+            Directory.CreateDirectory("c:/barras generadas")
+        End If
+
+        '--- Efectos Comienzo
+        Cursor = Cursors.WaitCursor
+        BtnGenerar.Enabled = False
+        txtinicio.Text = Val(txtinicio.Text) - 1
+        '--- Itero la cantidad de veces del limite
+        For a = Val(txtinicio.Text) To Val(txtfinal.Text) - 1
+
+            If txtinicio.Text > txtfinal.Text Then
+
+            Else
+                '---- Variables
+                NombreArchivo.Text = Val(NombreArchivo.Text) + 1
+                txtinicio.Text = Val(txtinicio.Text) + 1
+                num = 12 - txtinicio.TextLength
+            End If
+
+            '--- Imagen del control
+
+
+            '---- Generacion Barra
+            Dim iSum As Integer = 0
+            Dim iDigit As Integer = 0
+
+
+            '--- Completo los 0 que faltan al numero
+
+            If txtinicio.TextLength = 12 Then
+                CodigoBarra.Value = txtinicio.Text
+            Else
+
+                For i As Integer = 1 To num
+                    txtinicio.Text = "0" + txtinicio.Text
+                Next
+
+                If txtinicio.TextLength = 12 Then
+                    CodigoBarra.Value = txtinicio.Text
+                End If
+            End If
+
+            Dim CapturaBarra As Bitmap = getBitmap(CodigoBarra)
+
+            '--- Completo los 0 que faltan al nombre del archivo
+            If NombreArchivo.TextLength = 12 Then
+                CodigoBarra.Value = txtinicio.Text
+            Else
+
+                For i As Integer = 1 To num
+                    NombreArchivo.Text = "0" + NombreArchivo.Text
+                Next
+
+            End If
+
+            '--- Muestro Numero Completo con Cheksum
+            CodigoBarra.ShowBarcodeText = True
+            CodigoBarra.ShowCheckSum = True
+
+            '--- Muestra y Save de la Barra
+            CapturaBarra.Save("C:\Barras Generadas\" + NombreArchivo.Text + ".png")
+            Muestra.Image = CapturaBarra
+
+        Next a
+
+        '--- Limpio Textboxs
+        txtinicio.Clear()
+        txtfinal.Clear()
+
+        '--- Efectos despues del proceso
+        BtnGenerar.Enabled = True
+        Cursor = Cursors.Default
+        Process.Start("explorer.exe", "c:\Barras Generadas")
+
+    End Sub
+
+    Private Sub AcercadeToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles AcercadeToolStripMenuItem.Click
+
+        frmAyuda.Show()
+
+    End Sub
+
+    Private Sub txtinicio_KeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs) Handles txtinicio.KeyPress
+
+        Dim KeyAscii As Short = CShort(Asc(e.KeyChar))
+        KeyAscii = CShort(SoloNumeros(KeyAscii))
+        If KeyAscii = 0 Then
+            e.Handled = True
+        End If
+
+    End Sub
+
+    Private Sub txtfinal_KeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs) Handles txtfinal.KeyPress
+
+        Dim KeyAscii As Short = CShort(Asc(e.KeyChar))
+        KeyAscii = CShort(SoloNumeros(KeyAscii))
+        If KeyAscii = 0 Then
+            e.Handled = True
+        End If
+
+    End Sub
+
+End Class
